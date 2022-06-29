3f9cb3fffb32b1e1cfa191effeb05df7de474a10
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-29 22:21:26 +0700

    history

--- pages/history.js
+++ pages/history.js
@@ -5,8 +5,36 @@ import Header from '../components/Header'
 import { AmazonContext } from '../context/AmazonContext'
 
 const history = () => {
+  const { ownedItems } = useContext(AmazonContext)
+
+  const styles = {
+    container: `h-full w-full flex bg-[#fff]`,
+    main: `w-full h-full flex flex-col mt-[50px]`,
+    tableContainer: `w-full h-full flex flex-col p-[100px] justify-center`,
+    pageTitle: `text-2xl font-bold text-left mt-[50px] mb-[30px]`,
+    transactions: `flex gap-[50px] flex-row flex-wrap`,
+  }
+
   return (
-    <div>history</div>
+    <div className={styles.container}>
+      <Sidebar />
+
+      <div className={styles.main}>
+        <Header />
+        <div className={styles.tableContainer}>
+          {ownedItems ? (
+            <div className={styles.pageTitle}>Purchase History</div>
+          ) : (
+            <div className={styles.pageTitle}>No Purchase History</div>
+          )}
+          <div className={styles.transactions}>
+            {ownedItems.map((item, index) => {
+              return <Transaction key={index} item={item} index={index} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
